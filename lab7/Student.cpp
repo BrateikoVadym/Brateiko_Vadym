c06e8137b8c84a39b7ec2e2ad042d80202eb3649
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-27 13:12:54 +0200

    Create Student.cpp

---
+++ lab7/Student.cpp
@@ -0,0 +1,71 @@
+#include "student.h"
+
+// Конструктор
+Student::Student() {}
+
+// Деструктор
+Student::~Student() {
+    clearStudents();
+}
+
+// Додавання студента
+void Student::addStudent(int id, const std::string& name, int course) {
+    if (course <= 0 || course > 6) {
+        std::cout << "Помилка: Невалідний номер курсу!\n";
+        return;
+    }
+
+    students.emplace_back(id, name, course);
+}
+
+// Видалення студента за ID
+bool Student::removeStudent(int id) {
+    for (auto it = students.begin(); it != students.end(); ++it) {
+        if (it->id == id) {
+            students.erase(it);
+            return true;
+        }
+    }
+
+    std::cout << "Студента з ID " << id << " не знайдено.\n";
+    return false;
+}
+
+// Оновлення курсу студента
+bool Student::updateStudentCourse(int id, int new_course) {
+    if (new_course <= 0 || new_course > 6) {
+        std::cout << "Помилка: Невалідний номер курсу!\n";
+        return false;
+    }
+
+    for (auto& student : students) {
+        if (student.id == id) {
+            student.course = new_course;
+            return true;
+        }
+    }
+
+    std::cout << "Студента з ID " << id << " не знайдено.\n";
+    return false;
+}
+
+// Виведення списку студентів
+void Student::printStudents() const {
+    if (students.empty()) {
+        std::cout << "Список студентів порожній.\n";
+        return;
+    }
+
+    std::cout << "Список студентів:\n";
+    for (const auto& student : students) {
+        std::cout << "ID: " << student.id
+                  << ", Ім'я: " << student.name
+                  << ", Курс: " << student.course << std::endl;
+    }
+}
+
+// Очищення списку студентів
+void Student::clearStudents() {
+    students.clear();
+    std::cout << "Список студентів очищено.\n";
+}

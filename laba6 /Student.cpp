53897888fdf20e10caa2eb8786bb5065f5b0fe59
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-27 13:02:30 +0200

    Create Student.cpp

---
+++ laba6 /Student.cpp
@@ -0,0 +1,64 @@
+#include "student.h"
+
+void Student::inputStudent() {
+    if (count >= SIZE) {
+        std::cout << "Досягнуто максимальну кількість студентів!\n";
+        return;
+    }
+
+    student_s_t s;
+
+    std::cout << "Введіть ID: ";
+    std::cin >> s.id;
+
+    std::cout << "Введіть ім'я: ";
+    std::cin.ignore();
+    std::getline(std::cin, s.name);
+
+    // Перевірка курсу
+    std::cout << "Введіть курс (1-4): ";
+    std::cin >> s.course;
+
+    if (s.course < 1 || s.course > 4) {
+        std::cout << "Некоректний курс! Курс має бути від 1 до 4.\n";
+        return;
+    }
+
+    // Перевірка оцінки
+    std::cout << "Введіть оцінку (0-100): ";
+    std::cin >> s.grade;
+
+    if (s.grade < 0 || s.grade > 100) {
+        std::cout << "Некоректна оцінка! Оцінка має бути в діапазоні 0-100.\n";
+        return;
+    }
+
+    students[count++] = s;
+    std::cout << "Студента успішно додано!\n";
+}
+
+void Student::printStudents() const {
+    for (size_t i = 0; i < count; i++) {
+        std::cout << "ID: " << students[i].id
+                  << ", Ім'я: " << students[i].name
+                  << ", Курс: " << students[i].course
+                  << ", Оцінка: " << students[i].grade << '\n';
+    }
+}
+
+void Student::updateGrade(int id, int newGrade) {
+    if (newGrade < 0 || newGrade > 100) {
+        std::cout << "Некоректна оцінка! Оцінка має бути в діапазоні 0-100.\n";
+        return;
+    }
+
+    for (size_t i = 0; i < count; i++) {
+        if (students[i].id == id) {
+            students[i].grade = newGrade;
+            std::cout << "Оцінка оновлена.\n";
+            return;
+        }
+    }
+
+    std::cout << "Студента з таким ID не знайдено!\n";
+}

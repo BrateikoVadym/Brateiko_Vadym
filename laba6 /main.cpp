22384f000323ced385dc9a5978b93833348bfced
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-27 13:01:12 +0200

    Create main.cpp

---
+++ laba6 /main.cpp
@@ -0,0 +1,46 @@
+#include "student.h"
+
+int main() {
+    Student group;
+    int choice;
+
+    do {
+        std::cout << "\nМеню:\n";
+        std::cout << "1. Додати студента\n"
+                  << "2. Показати студентів\n"
+                  << "3. Оновити оцінку\n"
+                  << "0. Вийти\n"
+                  << "Ваш вибір: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                group.inputStudent();
+                break;
+
+            case 2:
+                group.printStudents();
+                break;
+
+            case 3: {
+                int id, grade;
+                std::cout << "Введіть ID студента: ";
+                std::cin >> id;
+                std::cout << "Введіть нову оцінку: ";
+                std::cin >> grade;
+
+                group.updateGrade(id, grade);
+                break;
+            }
+
+            case 0:
+                std::cout << "Вихід з програми...\n";
+                break;
+
+            default:
+                std::cout << "Некоректний вибір!\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}

71a4d2adb884478e0cd4dd4e93c43c5ea19328e7
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-27 13:03:04 +0200

    Create Student.h

---
+++ laba6 /Student.h
@@ -0,0 +1,30 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <array>
+#include <string>
+#include <iostream>
+
+const size_t SIZE = 100;
+
+struct student_s_t {
+    int id;
+    std::string name;
+    int course;
+    int grade;
+};
+
+class Student {
+private:
+    std::array<student_s_t, SIZE> students;
+    size_t count;
+
+public:
+    Student() : count(0) {}
+
+    void inputStudent();
+    void printStudents() const;
+    void updateGrade(int id, int newGrade);
+};
+
+#endif // STUDENT_H

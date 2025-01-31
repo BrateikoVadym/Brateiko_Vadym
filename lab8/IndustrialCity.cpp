c6ea35c681e9e24075a14062c1c674a27efac793
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-31 16:55:14 +0200

    Create IndustrialCity.cpp

---
+++ lab8/IndustrialCity.cpp
@@ -0,0 +1,9 @@
+#include "IndustrialCity.h"
+#include <iostream>
+
+IndustrialCity::IndustrialCity(string name, int population) : City(name, population) {}
+
+void IndustrialCity::displayData() const {
+    cout << "Industrial City - ";
+    City::displayData();
+}

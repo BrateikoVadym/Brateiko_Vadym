ff44d641faae9897585186173e8f8c4f1da8e0a3
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-31 16:50:42 +0200

    Create CulturalCity.h

---
+++ lab8/CulturalCity.h
@@ -0,0 +1,12 @@
+#ifndef CULTURALCITY_H
+#define CULTURALCITY_H
+
+#include "City.h"
+
+class CulturalCity : public City {
+public:
+    CulturalCity(string name, int population);
+    void displayData() const override;
+};
+
+#endif // CULTURALCITY_H

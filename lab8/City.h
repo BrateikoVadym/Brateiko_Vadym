074945a14faaa6626d1d49152db974f90728ed38
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-31 16:48:42 +0200

    Create City.h

---
+++ lab8/City.h
@@ -0,0 +1,20 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include <string>
+using namespace std;
+
+class City {
+public:
+    City(string name, int population);
+    virtual ~City();
+
+    virtual void inputData();
+    virtual void displayData() const;
+
+protected:
+    string name;
+    int population;
+};
+
+#endif // CITY_H

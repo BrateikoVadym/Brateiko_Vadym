631ae0d2dc8f35ffc8dd5bc88578c41570b5b1a8
BrateikoVadym <vadym.brateiko.ap.2023@lpnu.ua>
2025-01-31 16:53:26 +0200

    Create IndustrialCity.h

---
+++ lab8/IndustrialCity.h
@@ -0,0 +1,12 @@
+#ifndef INDUSTRIALCITY_H
+#define INDUSTRIALCITY_H
+
+#include "City.h"
+
+class IndustrialCity : public City {
+public:
+    IndustrialCity(string name, int population);
+    void displayData() const override;
+};
+
+#endif // INDUSTRIALCITY_H
